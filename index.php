016ced6cc1f492b91ed43a4a29df69b650532db7
Rajat k <rj_rajat18@yahoo.in>
2016-08-12 09:26:03 +0530

    Adds new Author

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="A simple claendar generator">
-    <meta name="author" content="Tushar Agarwal">
+    <meta name="author" content="Rajat">
     <title>Simple Calendar Generator</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/jquery.stepify.css" rel="stylesheet">
